fa872cae628990725418aec894f84b2da62b1638
tharlys10 <tharlys2015c@gmail.com>
2021-11-11 23:58:01 -0300

    feat: :sparkles: implementate total in return find in interface repository cities

--- src/modules/cities/repositories/ICitiesRepository.ts
+++ src/modules/cities/repositories/ICitiesRepository.ts
@@ -2,8 +2,13 @@ import { ICreateCityDTO } from '../dtos/ICreateCityDTO';
 import { City } from '../infra/typeorm/entities/City';
 
 interface ICitiesRepository {
-  find(name: string, state: string, limit: number, page: number): Promise<City>;
-  findById(id: string): Promise<City>;
+  find(
+    name: string,
+    state: string,
+    limit: number,
+    page: number
+  ): Promise<{ cities: City[]; total: number }>;
+  findById(id: string): Promise<City | undefined>;
   findByState(state: string): Promise<City[]>;
   create(data: ICreateCityDTO): Promise<City>;
 }
