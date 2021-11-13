60342ef7432888ba5dfd3077f58449eef5cf63f1
tharlys10 <tharlys2015c@gmail.com>
2021-11-13 00:30:27 -0300

    feat: :sparkles: create migration clients

---
+++ src/shared/infra/typeorm/migrations/1636773634669-CreateClients.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateClients1636773634669 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'clients',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+          },
+          {
+            name: 'gender',
+            type: 'varchar',
+          },
+          {
+            name: 'data_nasc',
+            type: 'date',
+          },
+          {
+            name: 'age',
+            type: 'integer',
+          },
+          {
+            name: 'city_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCityClient',
+            referencedTableName: 'cities',
+            referencedColumnNames: ['id'],
+            columnNames: ['city_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('clients');
+  }
+}
