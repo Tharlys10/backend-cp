9748ec71b3946ba5278d09ec3852c734291c81e2
tharlys10 <tharlys2015c@gmail.com>
2021-11-13 17:57:03 -0300

    test: :white_check_mark: test unitary: should not be able create new client if city not exists

--- src/modules/clients/useCases/createClient/CreateClientUseCase.spec.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { CitiesRepositoryInMemory } from '@modules/cities/repositories/in-memory/CitiesRepositoryInMemory';
 import { ClientsRepositoryInMemory } from '@modules/clients/repositories/in-memory/ClientsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 import { CreateClientUseCase } from './CreateClientUseCase';
 
 let clientsRepositoryInMemory: ClientsRepositoryInMemory;
@@ -17,14 +18,33 @@ describe('Create Client', () => {
   });
 
   it('should be able create new client', async () => {
+    const { id: city_id } = await citiesRepositoryInMemory.create({
+      name: 'San Diego',
+      state: 'California',
+    });
+
     const client = await createClientUseCase.execute({
       full_name: 'Fernanda Alícia Costa',
       gender: 'masculine',
       date_nasc: new Date('1980-01-23'),
       age: 41,
-      city_id: '5b5ade31-4400-4309-be97-a3583bc24234',
+      city_id,
     });
 
     expect(client).toHaveProperty('id');
+  });
+
+  it('should not be able create new client if city not exists', async () => {
+    const city_id = '5b5ade31-4400-4309-be97-a3583bc24234';
+
+    await expect(
+      createClientUseCase.execute({
+        full_name: 'Luiz Raimundo José Caldeira',
+        gender: 'masculine',
+        date_nasc: new Date('2001-08-10'),
+        age: 20,
+        city_id,
+      })
+    ).rejects.toEqual(new AppError('City not found'));
   });
 });
