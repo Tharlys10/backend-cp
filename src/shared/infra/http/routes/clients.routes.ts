8882365cc307f588bc2b642cd8de1c7a519b8ba8
tharlys10 <tharlys2015c@gmail.com>
2021-11-13 18:04:31 -0300

    feat: :sparkles: implementation router for create client

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,5 +1,8 @@
+import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
 import { Router } from 'express';
 
 const clientsRouter = Router();
+
+clientsRouter.post('/', new CreateClientController().handle);
 
 export { clientsRouter };
