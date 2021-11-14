dfe185847339cca0ec23556c97d1ddec76ce6c07
tharlys10 <tharlys2015c@gmail.com>
2021-11-14 13:23:19 -0300

    feat: :sparkles: implementation router find clients

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,8 +1,10 @@
+import { FindCitiesController } from '@modules/cities/useCases/findCities/FindCitiesController';
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
 import { Router } from 'express';
 
 const clientsRouter = Router();
 
+clientsRouter.get('/', new FindCitiesController().handle);
 clientsRouter.post('/', new CreateClientController().handle);
 
 export { clientsRouter };
