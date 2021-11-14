4a892a9ed1e092e4eae4fad93bf3598d180a7846
tharlys10 <tharlys2015c@gmail.com>
2021-11-14 14:18:13 -0300

    test: :white_check_mark: test integration: should be able search client by id

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,10 +1,12 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { FindClientByIdController } from '@modules/clients/useCases/findClientById/FindClientByIdController';
 import { FindClientsController } from '@modules/clients/useCases/findClients/FindClientsController';
 import { Router } from 'express';
 
 const clientsRouter = Router();
 
 clientsRouter.get('/', new FindClientsController().handle);
+clientsRouter.get('/:id', new FindClientByIdController().handle);
 clientsRouter.post('/', new CreateClientController().handle);
 
 export { clientsRouter };
