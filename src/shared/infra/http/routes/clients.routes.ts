e3a3e20f29c38ab24be76ff323558198846751a9
tharlys10 <tharlys2015c@gmail.com>
2021-11-14 15:19:25 -0300

    feat: :sparkles: implementation router for delte cliente

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,4 +1,5 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { DeleteClientController } from '@modules/clients/useCases/deleteClient/DeleteClientController';
 import { FindClientByIdController } from '@modules/clients/useCases/findClientById/FindClientByIdController';
 import { FindClientsController } from '@modules/clients/useCases/findClients/FindClientsController';
 import { Router } from 'express';
@@ -8,5 +9,6 @@ const clientsRouter = Router();
 clientsRouter.get('/', new FindClientsController().handle);
 clientsRouter.get('/:id', new FindClientByIdController().handle);
 clientsRouter.post('/', new CreateClientController().handle);
+clientsRouter.delete('/:id', new DeleteClientController().handle);
 
 export { clientsRouter };
